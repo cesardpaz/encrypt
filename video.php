8925b52aef0ce1b7b73133878056c57d5cea25db
César De Paz <cesardpaz@gmail.com>
2023-04-21 10:05:58 -0500

    remove files unnecessary

--- video.php
+++ video.php
@@ -1,8 +1,7 @@
-
 <?php
 if (isset($_GET['link'])) {
     $encodedLink = $_GET['link'];
     $link = base64_decode($encodedLink);
 ?>
-    <iframe id="IFR" src="<?php echo $link; ?>"frameborder="0" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>
+    <iframe  src="<?php echo $link; ?>" frameborder="0" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>
 <?php } ?>
